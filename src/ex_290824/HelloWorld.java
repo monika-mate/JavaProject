7c070f55b80c6edf752951d6142c629a6df8b08f
My Name <myEmail@example.com>
2024-08-29 07:26:54 +0530

    First Commit added

---
+++ src/ex_290824/HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_290824;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Monika");
+    }
+}
---
+++ src/ex_290824/Test001.java
@@ -0,0 +1,4 @@
+package ex_290824;
+
+public class Test001 {
+}
