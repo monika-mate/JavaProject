09a38d8b2349b5a6d4a6521d58a8ea8a7cd9f0a8
My Name <myEmail@example.com>
2024-08-30 14:31:32 +0530

    Add Test002

--- src/ex_290824/Test002.java
+++ src/ex_290824/Test002.java
@@ -1,4 +1,15 @@
 package ex_290824;
 
 public class Test002 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b =  a++;
+        System.out.println(b);
+        System.out.println(a);
+    }
+
+
+
+
+
 }
